060b3e68a9343f8f74eb872afcf4f456898cf16f
阿白smile <bingchengsu@gmail.com>
2020-03-27 12:04:30 +0800

    update

--- demos/vueTheory/vNode/vnode.js
+++ demos/vueTheory/vNode/vnode.js
@@ -1,29 +1,22 @@
-/**
- * type: 元素类型
- */
-function createElement(type, props = {}, ...children) {
-	console.log(type)
-	let key
-	if (props.key) {
-		key = props.key
-		delete props.key
-	}
-	children = children.map(child => {
-		if (typeof child === 'string') {
-			return vnode(undefined, undefined, undefined, undefined, child)
-		} else {
-			return child
-		}
-	})
-	return vnode(type, props, key, children)
+function createElement(tag, props, children) {
+  let key
+  if (props.key) {
+      key = props.key
+      delete props.key
+  }
+  return new Element(tag, props, key, children)
 }
 
-function vnode(type, props, key, children, text) {
-	return {
-		type,
-		props,
-		key,
-		children,
-		text
-	}
+function createTextVnode(str) {
+  return new Element(undefined, undefined, undefined, undefined, str)
+}
+
+class Element {
+  constructor(tag, props, key, children, text) {
+      this.tag = tag
+      this.props = props
+      this.key = key
+      this.children = children
+      this.text = text
+  }
 }
